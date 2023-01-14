d0a7ca5be083d25d9eadb1a704c557fe25a7f478
Alexander A Guerra <alexander.a.guerra@gmail.com>
2023-01-14 01:09:51 -0600

    Snapshot

--- components/Die.js
+++ components/Die.js
@@ -1,13 +1,37 @@
 import React from 'react'
 
-export default function Die() {
+export default function Die({ value, active, keepButtonHandler }) {
+    const ActiveFace = ({ value }) => {
+        return (
+            <div className="w-24 h-24 bg-slate-800 mx-auto">
+                <p className="text-2xl">{value}</p>
+                <p>asdf</p>
+            </div>
+        )
+    }
+
+    const InactiveFace = ({ value }) => {
+        return (
+            <div className="w-20 h-20 bg-slate-500 mx-auto">
+                <p className="text-2xl">{value}</p>
+                <p>asdf</p>
+            </div>
+        )
+    }
+
     return (
         <div className="bg-white text-center border-amber-500 border-4 flex flex-col justify-center shadow-xl">
-            <div className="w-20 h-20 bg-slate-600 mx-auto">
-                <p className="text-2xl">1</p>
-            </div>
+            {/* <div className="w-20 h-20 bg-slate-600 mx-auto">
+                <p className="text-2xl">{value}</p>
+            </div> */}
 
-            <button className="border-violet-400 border-4 font-sans rounded bg-slate-800 text-yellow-200 w-20 rotate-12">
+            {/* <ActiveFace value={value}></ActiveFace> */}
+
+            {active ? <ActiveFace></ActiveFace> : <InactiveFace></InactiveFace>}
+            <button
+                className="border-violet-400 border-4 font-sans rounded bg-slate-800 text-yellow-200 w-20 rotate-12"
+                onClick={keepButtonHandler}
+            >
                 Keep
             </button>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 import Head from 'next/head'
 
 import Die from '../components/Die'
 
 export default function Home() {
+    const [hidden, setHidden] = useState(true)
+    const [dice, setDice] = useState([
+        { id: 1, value: 1, active: true, kept: false },
+        { id: 2, value: 2, active: true, kept: false },
+        { id: 3, value: 3 },
+        { id: 4, value: 1 },
+        { id: 5, value: 1 },
+    ])
+
+    const startButtonHandler = () => {
+        console.log(hidden)
+        setHidden(!hidden)
+    }
     return (
         <>
             <Head>
@@ -16,18 +30,51 @@ export default function Home() {
             </Head>
             <main className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
                 <div className="container">
-                    <button className="bg-black text-white m-4 p-4">
+                    <button
+                        className="bg-black text-white m-4 p-4"
+                        onClick={startButtonHandler}
+                    >
                         Start
                     </button>
                 </div>
 
                 <div className="w-2/3 bg-green-700 grid grid-cols-3 gap-4 border-black border-4 rounded p-8 container mx-auto">
-                    <Die></Die>
-                    <Die></Die>
-                    <Die></Die>
-                    <Die></Die>
-                    <Die></Die>
+                    {dice.map((die) => {
+                        return (
+                            <div>
+                                <Die
+                                    key={die.id}
+                                    value={die.value}
+                                    active={die.active}
+                                    keepButtonHandler={() => {
+                                        console.log('The id is... ', die.id)
+                                    }}
+                                ></Die>
+                            </div>
+                        )
+                    })}
                 </div>
+
+                {dice
+                    .filter((die) => die.active)
+                    .map((die) => {
+                        return <Die key={die.id} value={die.value}></Die>
+                    })}
+
+                {dice
+                    .filter((die) => !die.active)
+                    .map((die) => {
+                        return <Die key={die.id} value={die.value}></Die>
+                    })}
+                {hidden ? (
+                    <h1 className="transition ease-in-out delay-150 duration-300 animate-pulse hover:-scale-100 w-20 bg-red-400 text-center">
+                        Hello
+                    </h1>
+                ) : (
+                    <h1 className=" transition-opacity ease-in duration-150 active:-scale-y-125 bg-gray-500">
+                        No
+                    </h1>
+                )}
             </main>
         </>
     )
